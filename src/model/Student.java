7c5e1e915e2677d85e65bb41301bfc291f0da69b
c1909i2vanh <truonggiang2298@gmail.com>
2020-10-22 18:35:20 +0700

    Complete

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,12 +5,21 @@
  */
 package model;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import static java.sql.DriverManager.getConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetProvider;
 
 /**
  *
@@ -24,11 +33,15 @@ public class Student {
     private String address;
     private String phoneNumber;
     private int gender;
-    private static final String DB_URL = "jddc:sqlserver://localhost:1433;databaseName=qlsv";
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=qlsv";
     private static final String USERNAME = "sa";
     private static final String PASSWORD = "1";
+    static Connection conn = null;
+    PreparedStatement pstmt = null;
+    CachedRowSet rowset = null;
 
     public Student() {
+
     }
 
     public Student(int id, String rollNumber, String name, String address, String phoneNumber, int gender) {
@@ -88,17 +101,314 @@ public class Student {
         this.gender = gender;
     }
 
-    public Connection getConnectionStudent() {
-        Connection conn = null;
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    /**
+     * * Connection to DB
+     *
+     * @return instance conn of Connection interface
+     */
+    private static Connection getConnectionStudent() {
 
-            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        try {
+            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = getConnection(DB_URL, USERNAME, PASSWORD);
             System.out.println("Ket noi thanh cong");
 
-        } catch (SQLException | ClassNotFoundException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn;
+    }
+
+    /**
+     *
+     */
+    public void setConnection() {
+        getConnectionStudent();
+    }
+
+    /**
+     * * display all data in tblsinhvien table
+     *
+     * @throws SQLException
+     */
+    public void displayData() throws SQLException {
+        try {
+            if (conn != null) {
+                System.out.println("ok");
+            }
+            String sql = "SELECT * FROM tblsinhvien";
+            pstmt = conn.prepareStatement(sql);
+            //rowset = pstmt.execute();
+            RowSetFactory factory = RowSetProvider.newFactory();
+            CachedRowSet cr = factory.createCachedRowSet();
+            cr.setCommand(sql);
+            cr.execute(conn);
+            while (cr.next()) {
+                if (cr.isFirst()) {
+                    System.out.printf("%-5s %-10s %-20s %-20s %-20s %-20s\n", "STT", "MA SV", "Ten", "Dia chi", "So dien thoai", "Gioi tinh");
+                    System.out.printf("%-5s %-10s %-20s %-20s %-20s %-20s\n", cr.getRow(), cr.getString("rollNumber"), cr.getString("name"), cr.getString("address"), cr.getString("phone"), (cr.getBoolean("gender")) ? "Nam" : "Nu");
+                } else {
+                    System.out.printf("%-5s %-10s %-20s %-20s %-20s %-20s\n", cr.getRow(), cr.getString("rollNumber"), cr.getString("name"), cr.getString("address"), cr.getString("phone"), (cr.getBoolean("gender")) ? "Nam" : "Nu");
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void validateData() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Nhap ma sinh vien: ");
+        do {
+            String regexRoll = "^C[0-9]{1,9}$";
+            Pattern patternRoll = Pattern.compile(regexRoll);
+            String strRoll = scan.nextLine().trim();
+            if (strRoll == null) {
+                System.err.println("Ma sinh vien khong duoc de trong! ");
+            } else {
+                Matcher matchRoll = patternRoll.matcher(strRoll);
+                if (matchRoll.matches()) {
+                    this.setRollNumber(strRoll);
+                    break;
+                } else {
+                    System.err.println("Ma sinh vien co kieu la Cxxx voi x la cac so tu 0-9! Vui long nhap lai!");
+                }
+            }
+        } while (true);
+
+        System.out.println("Nhap ten sinh vien: ");
+        do {
+            String strName = scan.nextLine().trim();
+            if (!strName.isEmpty() || strName != null) {
+                this.setName(strName);
+                break;
+            } else {
+                System.err.println("Ten sinh vien khong duoc de trong!");
+            }
+        } while (true);
+
+        System.out.println("Nhap dia chi: ");
+        this.setAddress(scan.nextLine());
+        System.out.println("Nhap phone: ");
+        do {
+            String strPhone = scan.nextLine();
+            String regexPhone = "^0[0-9]{9,10}$";
+            Pattern p = Pattern.compile(regexPhone);
+            Matcher matchPhone = p.matcher(strPhone);
+            if (matchPhone.matches()) {
+                this.setPhoneNumber(strPhone);
+                break;
+            } else {
+                System.err.println(" So dien thoai khong dung dinh dang! Vui long nhap lai!");
+                System.err.println("Vi du: 0123456789");
+            }
+
+        } while (true);
+
+        System.out.println("Nhap gioi tinh: 1 Nam | 0 Nu ");
+        do {
+            try {
+                int genderNumber = Integer.parseInt(scan.nextLine());
+                if (genderNumber == 1 || genderNumber == 0) {
+                    this.setGender(genderNumber);
+                    break;
+                } else {
+                    System.err.println("Vui long chon 1 hoac 0! Xin thu lai");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Vui long nhap vao 1 so nguyen! Vui long thu lai");
+            }
+        } while (true);
+    }
+
+    /**
+     * * Input multiple Student to tblsinhvien table
+     *
+     * @throws java.sql.SQLException
+     */
+    public void inputData() {
+
+        try {
+            Scanner scan = new Scanner(System.in);
+
+            //Khoa tu dong commit du lieu len server
+            boolean isStop = false;
+            String sql = "INSERT INTO tblsinhvien(rollNumber,name,address,phone,gender) VALUES(?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            conn.setAutoCommit(false);
+            Student st = new Student();
+            do {
+
+                st.validateData();
+                ps.setString(1, st.getRollNumber());
+                ps.setString(2, st.getName());
+                ps.setString(3, st.getAddress());
+                ps.setString(4, st.getPhoneNumber());
+                ps.setInt(5, st.getGender());
+                ps.addBatch();
+
+                System.out.println("Tiep tuc nhap tiep (1: CO| 2: Khong)");
+                int choice = 0;
+                do {
+                    try {
+                        choice = Integer.parseInt(scan.nextLine());
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.err.println("Vui long nhap vao 1 so nguyen!");
+                    }
+                } while (true);
+                if (choice != 1) {
+                    isStop = true;
+                }
+            } while (!isStop);
+            System.out.println(sql);
+            //Thuc thi thay doi
+            ps.executeBatch();
+            conn.setAutoCommit(true);
+        } catch (SQLException ex) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void inputDataToUpdate() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Nhap ma sinh vien: ");
+
+        this.setRollNumber(scan.nextLine());
+
+        System.out.println("Nhap ten sinh vien: ");
+        do {
+            String strName = scan.nextLine().trim();
+            if (strName != null) {
+                this.setName(strName);
+                break;
+            } else {
+                System.err.println("Ten sinh vien khong duoc de trong!");
+            }
+        } while (true);
+
+        System.out.println("Nhap dia chi: ");
+        this.setAddress(scan.nextLine());
+        System.out.println("Nhap phone: ");
+        do {
+            String strPhone = scan.nextLine();
+            String regexPhone = "^0[0-9]{9,10}$";
+            Pattern p = Pattern.compile(regexPhone);
+            Matcher matchPhone = p.matcher(strPhone);
+            if (matchPhone.matches()) {
+                this.setPhoneNumber(strPhone);
+                break;
+            } else {
+                System.err.println(" So dien thoai khong dung dinh dang! Vui long nhap lai!");
+                System.err.println("Vi du: 0123456789");
+            }
+
+        } while (true);
+
+        System.out.println("Nhap gioi tinh: 1 Nam | 0 Nu ");
+        do {
+            try {
+                int genderNumber = Integer.parseInt(scan.nextLine());
+                if (genderNumber == 1 | genderNumber == 0) {
+                    this.setGender(genderNumber);
+                    break;
+                } else {
+                    System.err.println("Vui long chon 1 hoac 0! Xin thu lai");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Vui long nhap vao 1 so nguyen! Vui long thu lai");
+            }
+        } while (true);
+    }
+
+    /**
+     * * Function update data by rollNumber
+     *
+     * @throws java.sql.SQLException
+     */
+    public void updateData() throws SQLException {
+
+        Student st = new Student();
+        st.validateData();
+        System.out.println("abc");
+        String sql = "UPDATE tblsinhvien set name =?,address =?, phone =?,gender =? where rollNumber =?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, st.getName());
+        ps.setString(2, st.getAddress());
+        ps.setString(3, st.getPhoneNumber());
+
+        ps.setInt(4, st.getGender());
+        ps.setString(5, st.getRollNumber());
+        int i = ps.executeUpdate();
+        System.out.println(i);
+
+        if (i > 0) {
+            System.out.println("Sua thanh cong " + i + " ban ghi");
+        } else {
+            System.out.println("Sua that bai! Vui long kiem tra lai du lieu");
+        }
+
+    }
+
+    public void deleteData(Scanner scan) {
+        try {
+
+            System.out.println("Nhap ma sinh vien muon xoa: ");
+            String rollDelNumber = scan.nextLine();
+            String sql = "DELETE FROM tblsinhvien WHERE rollNumber = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, rollDelNumber);
+            int row = ps.executeUpdate();
+            if (row > 0) {
+                System.out.println("Xoa thanh cong " + row + " hang");
+            } else {
+                System.err.println("Xoa that bai, vui long kiem tra lai du lieu");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void searchStudentById(Scanner scan) throws SQLException {
+        System.out.println("Nhap ma sinh vien muon tim: ");
+        String roll = scan.nextLine();
+        String sql = "{CALL sp_getStudent(?)}";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, roll);
+        ResultSet rs = ps.executeQuery();
+       boolean checkResultSet = false;
+      
+        while (rs.next()) {
+            checkResultSet= true;
+            System.out.printf("%-10s %-20s %-20s %-20s %-20s\n", "MA SV", "Ten", "Dia chi", "So dien thoai", "Gioi tinh");
+            do {                
+                 System.out.printf("%-10s %-20s %-20s %-20s %-20s\n", rs.getString("rollNumber"), rs.getString("name"), rs.getString("address"), rs.getString("phone"), (rs.getBoolean("gender")) ? "Nam" : "Nu");
+            } while (rs.next());        
+        }
+        if(!checkResultSet){
+            System.out.println("Khong tim thay sinh vien co ma la "+roll);
+        }
+
+    }
+
+    /**
+     * * Disconnect Connection JDBC
+     */
+    public void disconectJDBC() {
+        try {
+            conn.close();
+            System.out.println("Da dong DB! ");
+        } catch (Exception e) {
+        }
+
     }
 }
--- src/ss2_lab7_th1/App.java
+++ src/ss2_lab7_th1/App.java
@@ -8,6 +8,7 @@ package ss2_lab7_th1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Student;
@@ -20,29 +21,137 @@ public class App {
 
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
-    static Connection conn = null;
-    static DriverManager abc= null;
-    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=qlsv";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "1";
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Scanner scan = new Scanner(System.in);
+        App myApp = new App();
+        Student st = new Student();
+        st.setConnection();
+        int choice = 0;
         try {
-            Student st = new Student();
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-            System.out.println("dong");
+            do {
+                myApp.displayMenu(scan);
+                choice = getIntChoice(scan);
+                switch (choice) {
+                    case 1:
+                        myApp.inputData(st);
+                        break;
+                    case 2:
+                        myApp.displayData(st);
+                        break;
+                    case 3:
+                        myApp.updateData(st);
+                        break;
+                    case 4:
+                        myApp.deleteStudentById(st, scan);
+                        break;
+                    case 5:
+                        myApp.searchStudentById(st, scan);
+                        break;
+                    case 6:
+                        break;
+
+                }
+            } while (choice != 6);
+
+        } finally {
+            myApp.disconectDB(st);
+            System.exit(0);
+        }
+
+    }
+
+    private static int getIntChoice(Scanner scan) {
+        int number = 0;
+        do {
             try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                number = Integer.parseInt(scan.nextLine());
+                if (number > 0) {
+                    break;
+                } else {
+                    System.err.println("Vui long nhap vao 1 so lon hon 0!");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Vui long nhap vao 1 so nguyen");
             }
+        } while (true);
+        return number;
+    }
+
+    public void displayMenu(Scanner scan) {
+        System.out.println("****************MENU****************");
+        System.out.println("1. Nhập thông tin sinh viên");
+        System.out.println("2. Danh sách sinh viên");
+        System.out.println("3. Sửa sinh viên");
+        System.out.println("4. Xóa sinh viên");
+        System.out.println("5. Tìm sinh viên");
+        System.out.println("6. Thoát");
+        System.out.println("Vui lòng chọn từ 1 -> 6");
+    }
+
+    /**
+     *
+     ** Function display data of tblsinhvien table
+     *
+     * @param st
+     */
+    public void displayData(Student st) {
+        try {
+            st.displayData();
+        } catch (SQLException ex) {
         }
     }
 
+    /**
+     * @throws java.sql.SQLException
+     * @ Function inputData to table tblsinhvien
+     * @param st
+     */
+    public void inputData(Student st) throws SQLException {
+        st.inputData();
+    }
+
+    /**
+     * * Function update Data
+     *
+     * @param st
+     * @throws java.sql.SQLException
+     */
+    public void updateData(Student st)  throws SQLException {
+    
+            st.updateData();     
+      
+    }
+
+    /**
+     *
+     ** Function delete data of student by ID
+     *
+     * @param scan
+     * @param st
+     */
+    public void deleteStudentById(Student st, Scanner scan) {
+        st.deleteData(scan);
+    }
+
+    /**
+     *  * Function search Student by ID
+     *
+     * @param st
+     * @param scan
+     * @throws java.sql.SQLException
+     */
+    public void searchStudentById(Student st, Scanner scan) throws SQLException {
+        st.searchStudentById(scan);
+    }
+
+    /**
+     * * Function disconnect to server
+     *
+     * @param st
+     */
+    public void disconectDB(Student st) {
+        st.disconectJDBC();
+    }
 }
